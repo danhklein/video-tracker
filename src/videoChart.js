41efa37ad03b155fd8834214bdbbbd067a2f9d19
Daniel Klein <danhirschklein@gmail.com>
2016-09-23 01:04:51 -0600

    Working again

--- src/videoChart.js
+++ src/videoChart.js
@@ -1,32 +1,76 @@
-
-let myChart = new Chart(ctx, {
-  labels: [0, 2, vid.duration],
-  type: 'line',
-  data: {
+var ctx = document.getElementById("myChart");
+var data = {
+    labels: [],
     datasets: [
-
     ]
-  },
-  options: {
-    scales: {
-      xAxes: [
-        {
-          type: 'linear',
-          position: 'bottom'
-        }
-      ]
-    }
-  }
-})
+};
 
-let lineColors = ['rgba(255,255,0)', 'rgba(0,255,255)', 'rgb(255,0,255)', 'rgba(255,102,178)']
+var options = {
+  responsive:true,
+  maintainAspectRatio: true,
+  title: {
+  display: true,
+  text: 'Replay History'
+  },
+  elements: {
+    line: {
+      borderColor: 'rgba(0,0,128,0.7)'
+    }
+  },
+  scales: {
+      xAxes: [{
+        type:'linear',
+        position: 'bottom',
+        ticks: {
+
+          beginAtZero:true
+        }
+      }],
+      yAxes: [{
+          ticks: {
+              beginAtZero:true,
+              max: 5,
+              stepSize: 1
+          }
+      }]
+  }
+}
+
+
+
+// var options = {
+  // title: {
+  // display: true,
+  // text: 'Replay History'
+  // },
+  // elements: {
+  //   line: {
+  //     borderColor: 'rgba(0,0,128,0.7)'
+  //   }
+  // },
+//   scales: {
+//     xAxes: {
+//       type: 'linear',
+//       beginAtZero: true
+//     }
+//   }
+// }
+
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options});
+
+
+
 
 //Erase and Build New Chart
 function createChart () {
   myChart.update();
 }
 
-//Create all Datasets from Array
+// // //Create all Datasets from Array
 function insertAllDataIntoSets () {
   let line = [];
   for (var i = myChart.data.datasets.length; i<allPlayBacks.length; i++) {
@@ -39,44 +83,49 @@ function insertAllDataIntoSets () {
       x: allPlayBacks[i][1],
       y: replay
     }
+    myChart.data.labels.push(line[1].x)
     myChart.data.datasets.push({
       label: `Replay ${replay}`,
       fill: false,
       data: line
     })
+    if (myChart.data.datasets.length > 4) {
+      myChart.options.scales.yAxes[0].ticks.max = myChart.data.datasets.length + 1;
+    }
   }
   createChart();
 }
 
-//http://stackoverflow.com/questions/24943200/javascript-2d-array-indexof
-// function isItemInMatrix(array, item) {
-//   for (var i = 0; i < array.length; i++) {
-//         // This if statement depends on the format of your array
-//     if (array[i][0] == item) {
-//       return true;   // Found it
-//     }
-//   }
-//   return false;   // Not found
-// }
 
-let averageData = [];
+let averageDataArray = [];
 
 //NEXT STEP: Push the average data into the chart
 
 
-function recursion (sortedData, i){
-  var tempArray = [];
+//Organize Datasets Array into Array of sorted Points
+
+//Is the averageData Array empty? Push first sorted point into it.
+
+
+
+
+function fullBuild (datasets) {
+  sortedRanges()
+}
+
+function buildAverageDataArray (sortedData, i){
+  let tempArray = [];
   if (sortedData.length===1 && !i ){
-    //recursion over
+    return 0;
   } if (sortedData.length > 1) {
-  averageBuilder(sortedData[i], sortedData[sortedData.length-1], aveArray);
+  averageBuilder(sortedData[i], sortedData[sortedData.length-1], averageDataArray);
   tempArray.push(sortedData.pop());
-  tryAgain(sortedData, i);
+  recursion(sortedData, i);
   } if (sortedData.length===1 && i > 0) {
-    tryAgain(tempArray, i++)
+    recursion(tempArray, i++)
   }
 }
-//On each iteration
+// //On each iteration
 
 let averageBuilder = (range, point, aveArray) => {
   if(doRangesOverlap(range, point)) {
@@ -85,14 +134,14 @@ let averageBuilder = (range, point, aveArray) => {
   return 0;
 }
 
-//Called in averageBuilder()
+// //Called in averageBuilder()
 let doRangesOverlap = (range, point) => {
   if (range.data[0].x <= point.data[0].x && point.data[0].x <= range.data[1].x) {
     return 1;
   }
   return 0;
 }
-//Called in averageBuilder()
+// //Called in averageBuilder()
 let addAveragePoint = (point, aveArray) => {
    if (indexOfRow(averageArray, point.data[0]) === -1) {
     aveArray.push([point.data[0],1]);
@@ -100,7 +149,7 @@ let addAveragePoint = (point, aveArray) => {
   else aveArray[indexOfRow(averageArray, point.data[0])][1]++
 }
 
-//Called in addAveragePoint()
+// //Called in addAveragePoint()
 let indexOfRow = (array, item) => {
   for (var i = 0; i < array.length; i++) {
         // This if statement depends on the format of your array
@@ -110,7 +159,7 @@ let indexOfRow = (array, item) => {
   }
   return -1;   // Not found
 }
-//Creates new Array sorted by playback moment
+// //Creates new Array sorted by playback moment
 
 function sortRanges (datasets) {
   let mapped = datasets.map( function (el, i) {
@@ -121,4 +170,5 @@ function sortRanges (datasets) {
   });
   // container for the resulting order
   let result = mapped.map(function (el){ return datasets[el.index]});
-  return result}
+  return result
+}
